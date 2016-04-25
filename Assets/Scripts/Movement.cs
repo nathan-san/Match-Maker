4ba9a4f7f4d9e8967a01272e1308c21f57d9e700
jimbo333 <19198@ma-web.nl>
2016-04-25 16:44:22 +0200

    isGrounded and gravity added

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,15 +7,33 @@ public class Movement : MonoBehaviour {
     [SerializeField]
     private float jumpForce;
     private Vector2 movement = Vector2.zero;
+    private bool isGrounded = true;
+    private float ySpeed = 0;
+    private float gravity = 0.001f;
 
 	void FixedUpdate () {
-        movement = new Vector2(Input.GetAxis("Horizontal") * playerSpeed, 0);
+        movement = new Vector2(Input.GetAxis("Horizontal") * playerSpeed, ySpeed);
         transform.Translate(movement);
 
+        ySpeed -= gravity;
+
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            rb.AddForce(Vector2.up * jumpForce);
+            //rb.AddForce(Vector2.up * jumpForce);
+            ySpeed = jumpForce;
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        //Debug.Log("isGrounded");
+        if(coll.gameObject.tag == Tags.ground)
+        {
+            ySpeed = 0;
+            Debug.Log("isGrounded");
+            isGrounded = true;
         }
     }
 }
