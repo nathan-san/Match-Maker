8527f2c2b4e6e77b79253ebdea48d38791020f49
jimbo333 <19198@ma-web.nl>
2016-04-25 15:10:16 +0200

    Movement added

---
+++ Assets/Movement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    [SerializeField]
+    private float playerSpeed;
+    [SerializeField]
+    private float jumpForce;
+    private Vector2 movement = Vector2.zero;
+
+	void FixedUpdate () {
+        movement = new Vector2(Input.GetAxis("Horizontal") * playerSpeed, 0);
+        transform.Translate(movement);
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+    }
+}
