892a1015fd5f314306dfc22e99f167d8366a936b
nathan-san <18964@ma-web.nl>
2016-04-25 16:20:39 +0200

    added player in the main scene.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,19 +2,20 @@
 using System.Collections;
 
 public class Movement : MonoBehaviour {
-    private float moveSpeed;
-	// Use this for initialization
-	void Start () {
-	    
-	}
+    [SerializeField]
+    private float playerSpeed;
+    [SerializeField]
+    private float jumpForce;
+    private Vector2 movement = Vector2.zero;
 
-    
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(KeyCode.A))
+	void FixedUpdate () {
+        movement = new Vector2(Input.GetAxis("Horizontal") * playerSpeed, 0);
+        transform.Translate(movement);
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (Input.GetButtonDown("Jump"))
         {
-            
+            rb.AddForce(Vector2.up * jumpForce);
         }
-	}
+    }
 }
