0a9076c00f4a1bada19ce9aca9eed7c2bba327df
jimbo333 <jimmyberndsen@MacBook-Pro-van-Jimmy.local>
2016-05-09 14:29:01 +0200

    TimeLimit added

---
+++ Assets/Scripts/TimeLeft.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeLeft : MonoBehaviour {
+	[SerializeField]
+	private float timeLimit;
+	private Vector3 scale;
+	private float timeDevision;
+
+	void Start() {
+		scale = transform.localScale;
+		timeDevision = timeLimit / scale.x;
+	}
+
+	void Update () {
+		if (timeLimit > 0f) {
+			timeLimit -= Time.deltaTime;
+			scale.x = timeLimit / timeDevision;
+			transform.localScale = scale;
+		}
+	}
+}
