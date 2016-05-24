baacb8d7d1f79a76a6a8a3f997122fe064d47e3f
jimbo333 <jimmyberndsen@MacBook-Pro-van-Jimmy.local>
2016-05-24 10:21:29 +0200

    AudioController.cs added

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AudioController : MonoBehaviour {
+	[SerializeField]
+	private bool sound = true;
+
+	void Update () {
+		sound = GetComponent<Toggle> ().isOn;
+	}
+
+	public void AudioControl() {
+		if (sound == false) {
+			AudioListener.volume = 1;
+		} else if (sound == true) {
+			AudioListener.volume = 0;
+		}
+	}
+}
